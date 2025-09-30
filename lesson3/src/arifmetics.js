3f08e35df9e36343a42c9f68ddd1c7c234b21a54
havrybo <obsek131@gmail.com>
2025-09-30 12:54:36 +0300

    Completed Homework: lesson3 (edited)

--- lesson3/src/arifmetics.js
+++ lesson3/src/arifmetics.js
@@ -1,36 +1,36 @@
-const num1 = 10; 
-const num2 = 5;  
+const num1 = 10;
+const num2 = 5;
 
-const strNum = "6"; 
-const strText = "apples"; 
+const strNum = '6';
+const strText = 'apples';
 
 const boolValue = true;
 
-console.log("--- Operations on variables of the same type ---");
+console.log('--- Operations on variables of the same type ---');
 
-console.log(num1 + num2); 
-console.log(num1 - num2); 
+console.log(num1 + num2);
+console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
 console.log(num1 % num2);
 console.log(num2 ** 2);
 
-console.log("--- Modification and assignment ---");
+console.log('--- Modification and assignment ---');
 
 let a = 10;
 console.log(a);
-a += 5; 
+a += 5;
 console.log(a);
 a -= 10;
-console.log(a); 
-a *= 10; 
-console.log(a); 
+console.log(a);
+a *= 10;
+console.log(a);
 
-console.log("--- Operations on variables of different types ---");
+console.log('--- Operations on variables of different types ---');
 
-console.log(num1 + strNum); 
-console.log(num1 - strNum); 
-console.log(num1 * strNum); 
+console.log(num1 + strNum);
+console.log(num1 - strNum);
+console.log(num1 * strNum);
 console.log(num1 - strText);
 console.log(num1 + boolValue);
 console.log(num1 * !boolValue);
--- lesson3/src/decision-tree.js
+++ lesson3/src/decision-tree.js
@@ -1,24 +1,24 @@
-let age = 19;
-let hasPermission = true;
+const age = 19;
+const hasPermission = true;
 
 console.log('--- Checking access based on age and permission ---');
 
 if (age >= 18 && hasPermission) {
-    console.log("Access Approved");
+    console.log('Access Approved');
 } else {
-    console.log("Access Denied");
+    console.log('Access Denied');
 }
 
-let score = 73;
+const score = 93;
 
 console.log('--- Evaluating score category ---');
 
 if (score >= 90) {
-    console.log("Excellent");
+    console.log('Excellent');
 } else if (score >= 75) {
-    console.log("Good");
+    console.log('Good');
 } else if (score >= 50) {
-    console.log("Satisfactory");
+    console.log('Satisfactory');
 } else {
-    console.log("Needs Improvement");
+    console.log('Needs Improvement');
 }
