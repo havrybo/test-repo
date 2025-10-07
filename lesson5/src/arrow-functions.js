96d459cbd675496a5942ee59f4a0d5f34a768d23
havrybo <obsek131@gmail.com>
2025-10-07 22:59:08 +0300

    Home Work: lesson 5

---
+++ lesson5/src/arrow-functions.js
@@ -0,0 +1,27 @@
+const sumArrayValues = (arr) => {
+
+    if (!Array.isArray(arr)) {
+        console.log('The provided value is not an array');
+        return;
+    }
+
+    let sum = 0;
+
+    arr.forEach((element) => {
+        if (typeof element === 'number') {
+            sum += element;
+        } else if (typeof element === 'string' && !isNaN(Number(element))) {
+            sum += Number(element);
+        } else {
+            console.log(`Element '${element}' cannot be added`);
+        }
+    });
+
+    return sum;
+};
+
+const numArray = [5, 10, 15];
+const strArray = ['100', '200', 'apple'];
+
+console.log('Sum of numbers (arrow):', sumArrayValues(numArray));
+console.log('Sum of strings (arrow):', sumArrayValues(strArray));
